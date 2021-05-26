45146c09694fa16a3705ce2479b14d33fd1abe01
Roney-juma <ochirone10@gmail.com>
2021-05-26 14:35:52 +0300

    modified the app-routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GithubComponent } from './github/github.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "home", component: HomeComponent},
+  {path: "search",component: GithubComponent},
+  { path: 'users/:id', component: UserDetailsComponent },
+  {"pathMatch":"full","redirectTo":"/home","path":"" },
+  {path: "**",component: NotFoundComponent}
+
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
