9386ebbfd86f357f5695e6d7744180b21452d5d2
Roney-juma <ochirone10@gmail.com>
2021-05-26 13:34:16 +0300

    created class user

--- src/app/repository.ts
+++ src/app/repository.ts
@@ -1,5 +1,13 @@
 export class Repository {
-    constructor(public id: number,public name: string, public user: string,public link: string,public ownerPicture: string,public score: number,public language: string,public dateCreated: Date ){
+    constructor(
+        public id: number,
+        public name: string,
+        public user: string,
+        public link: string,
+        public ownerPicture: string,
+        public score: number,
+        public language: string,
+        public dateCreated: Date ){
 
     }
 }
